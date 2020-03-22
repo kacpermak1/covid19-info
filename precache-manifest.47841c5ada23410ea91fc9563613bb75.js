c2b898fcf6681e2f0b29af85939deddcfe9d1c6c
Kacper <kacpermak24@gmail.com>
2020-03-22 15:16:39 +0000

    Updates

---
+++ precache-manifest.47841c5ada23410ea91fc9563613bb75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "553989bdf142a52c819c6e255c6c92a1",
+    "url": "/covid19-info/index.html"
+  },
+  {
+    "revision": "7ce9d54bf7b9eeaef844",
+    "url": "/covid19-info/static/css/main.882b7a7b.chunk.css"
+  },
+  {
+    "revision": "e94b0c5de97d07253832",
+    "url": "/covid19-info/static/js/2.b8961a08.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/covid19-info/static/js/2.b8961a08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ce9d54bf7b9eeaef844",
+    "url": "/covid19-info/static/js/main.dbdcb03a.chunk.js"
+  },
+  {
+    "revision": "97a1215beb95e1b2b671",
+    "url": "/covid19-info/static/js/runtime-main.103896e4.js"
+  },
+  {
+    "revision": "389944ac10b513bb0abbf23f1c8bb795",
+    "url": "/covid19-info/static/media/virus1.389944ac.jpg"
+  }
+]);
